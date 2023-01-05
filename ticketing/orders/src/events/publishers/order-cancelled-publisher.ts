88d3d857d20525956b8ce630b78bb445f8dc244f
Golam Mostofa <golammostofa31188@gmail.com>
2023-01-05 16:14:05 +0600

    414 videoes completed and running perfectly

--- ticketing/orders/src/events/publishers/order-cancelled-publisher.ts
+++ ticketing/orders/src/events/publishers/order-cancelled-publisher.ts
@@ -1,4 +1,4 @@
-import { Subjects, Publisher, OrderCancelledEvent } from '@cygnetops/common';
+import { Subjects, Publisher, OrderCancelledEvent } from '@mostofa62/common';
 
 export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
   subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
--- ticketing/orders/src/events/publishers/order-created-publisher.ts
+++ ticketing/orders/src/events/publishers/order-created-publisher.ts
@@ -1,4 +1,4 @@
-import { Publisher, OrderCreatedEvent, Subjects } from '@cygnetops/common';
+import { Publisher, OrderCreatedEvent, Subjects } from '@mostofa62/common';
 
 export class OrderCreatedPublisher extends Publisher<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
