433753d09aff61274cf595f62f2cb9c8fc066a12
Golam Mostofa <golammostofa31188@gmail.com>
2023-01-15 16:38:24 +0600

    section-22 is done and complete operational

--- ticketing/orders/src/events/listeners/expiration-complete-listener.ts
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -3,21 +3,25 @@ import {
   Subjects,
   ExpirationCompleteEvent,
   OrderStatus,
-} from '@mostofa62/common';
-import { Message } from 'node-nats-streaming';
-import { queueGroupName } from './queue-group-name';
-import { Order } from '../../models/order';
-import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+} from "@mostofa62/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   queueGroupName = queueGroupName;
   subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
 
-  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
-    const order = await Order.findById(data.orderId).populate('ticket');
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId).populate("ticket");
 
     if (!order) {
       throw new Error('Order not found');
+    }
+
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
     }
 
     order.set({
